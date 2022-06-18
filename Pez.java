87de0811852a333dc621216055fae95002754c5c
Omar Cifuentes <omar.cifuentes.uv@gmail.com>
2022-06-17 21:58:33 -0400

    update3

--- Pez.java
+++ Pez.java
@@ -1,87 +1,90 @@
 
 public class Pez extends Problem {
-	
-	private int[] x = new int[nVars];
-	private double[] x_new = new double[nVars];
-	private int[] p = new int[nVars];
-	private double lambda = 0;
-	
-	
-	public Pez() {
-		for (int j = 0; j < nVars; j++) {
-			x[j] = StdRandom.uniform(2);	
-		}
-	}
-	
-	protected int computeFitness() {
-		return computeFitness(x);
-	}
 
-	protected int computeFitnessPBest() {
-		return computeFitness(p);
-	}
 
-	protected boolean isBetterThanPBest() {
-		return computeFitness() < computeFitnessPBest();
-	}
 
-	protected boolean isBetterThan(Pez g) {
-		return computeFitnessPBest() < g.computeFitnessPBest();
-	}
-	
-	protected boolean isFeasible() {
-		return checkConstraint(x);
-	}
-	
-	protected void updatePBest() {
-		System.arraycopy(x, 0, p, 0, x.length);
-	}
-	
-	
-	protected void copy(Object object) {
-		if (object instanceof Pez) {
-			System.arraycopy(((Pez) object).x, 0, x, 0, nVars);
-			((Pez) object).lambda = this.lambda;
-		}
-	}
-	
-	
-	protected void move(int A, double PD, double epsilon, int t, Pez g, double lambda  ) {
-		 
-//		lambda = (2 * StdRandom.uniform(0,1) * PD) - PD;
-		
-		for (int j = 0; j < nVars; j++) {
-			/* Actualizar posicion */
-			x_new[j] = g.x[j] - lambda * (StdRandom.uniform(0,1) * (g.x[j]+ 1/2)- x[j]);
-			x[j] = toBinary(x[j] + x_new[j]);
-		}
-		
-		
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("optimal value: %d, fitness: %d, diff: %.1f, rpd: %.2f%%, p: %s", optimum(),
-				computeFitness(x), diff(), rpd(), showSolution());
-	}
-	
-	private String showSolution() {
-		return java.util.Arrays.toString(x);
-	}
-	
-	private float diff() {
-		return computeFitness(x) - optimum();
-	}
+    //VARIABLES DEL PROBLEMA
+    private int[] x = new int[nVars];
+    private int[] p = new int[nVars];
+   
 
-	private float rpd() {
-		return diff() / optimum() * 100f;
-	}
-	
-	private int toBinary(double x) {
+    public Pez() {
+        for (int j = 0; j < nVars; j++) {
+            x[j] = StdRandom.uniform(2);
+
+        }
+
+    }
+    
+    protected int computeFitness() {
+        return computeFitness(x);
+    }
+
+    protected int computeFitnessPBest() {
+        return computeFitness(p);
+    }
+
+    protected boolean isFeasible() {
+        return checkConstraint(x);
+    }
+
+    protected boolean isBetterThanPBest() {
+        return computeFitness() < computeFitnessPBest(); //
+    }
+
+    protected boolean isBetterThan(Pez g) {
+        return computeFitness(x) < computeFitness(g.x); //compara solucion que tiene x y la va comparar con la de g
+    }
+
+    
+    private int toBinary(double x) {
 		return StdRandom.uniform() <= (1 / (1 + Math.pow(Math.E, -x))) ? 1 : 0;
 	}
-
 	
-	
+    
+    protected void SFmove(int A,  double epsilon, int t, Pez gSF , Pez gSD, double lamda, double alplha) {
+      
+        int i_sar= (int)alplha;
+        
+        //for (int j = 0; j < nVars; j++) {
+        for (int j = 0; j < i_sar; j++) {
 
+
+            x[j] = toBinary(gSF.x[j] - lamda * (StdRandom.uniform(0,1) * ((gSF.x[j]+ gSD.x[j])/2)- x[j]));
+
+        }
+    }
+    
+    protected void SDmove(int A,  double epsilon, int t, Pez gSF , Pez gSD, double lamda, double ataque) {
+
+        for (int j = 0; j < nVars; j++) {
+         
+             x[j] =  toBinary(StdRandom.uniform(0,1) * (gSF.x[j]-  gSD.x[j] + ataque));
+
+        }
+    }
+
+    protected void copy(Object object) {
+        if (object instanceof Pez) {
+            System.arraycopy(((Pez) object).x, 0, this.x, 0, nVars);
+        }
+    }
+
+    private float diff() {
+        return computeFitness(x) - optimum();
+    }
+
+    private float rpd() {
+        return diff() / optimum() * 100f;
+    }
+
+    private String showSolution() {
+        return java.util.Arrays.toString(x);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("optimal value: %d, fitness: %d, diff: %.1f, rpd: %.2f%%, p: %s", optimum(),
+                computeFitness(x), diff(), rpd(), showSolution());
+    }
 }
--- SwarmPez.java
+++ SwarmPez.java
@@ -1,122 +1,119 @@
 
 public class SwarmPez {
-	
-	private final int ps = 25;
-	private final int T = 1000;
-	private final int A = 4;
-	private final double epsilon = 0.01;
-	
-	
-	
-	private java.util.List<Pez> swarm = null;
-	private Pez g = null;
-	
-	public void execute() {
-		init();
-		evolve();
-	}
-	
-	private void init() {
-		swarm = new java.util.ArrayList<Pez>();
-		
-//		Se inicializa el primer Pez
-		
-		g = new Pez();
-		
-		Pez b;
-		
-		
-//		Se inicializa cada pez y se busca que sea factible
-		
-		for(int i = 1; i <= ps; i++) {
-			do {
-				
-//				Aca va el move 
-				
-				b = new Pez();	
-			} while(!b.isFeasible());
-			swarm.add(b);
-		}
-		
-//		StdOut.printf("Lleno el enjambre");
-		
-		
-		g.copy(swarm.get(0));
-		
-//		Se verifica que sea el mejor pez
-		
-		for (int i = 1; i < ps; i++) {
-			if (swarm.get(i).isBetterThan(g)) {
-				g.copy(swarm.get(i));
-			}
-		}
-		log(0);
-	}
-	
-	private void evolve() {
-		
-		int t = 1;
-		while (t <= T) {
-			Pez p = new Pez();
-			for (int i = 0; i < ps; i++) {
-				do {
-					p.copy(swarm.get(i));
-					
-					var PD = 1-(ps /(ps+250));
-					
-					var lambda = (2 * StdRandom.uniform(0,1) * PD) - PD;
-					
-					p.move(A, PD , epsilon, t, g, lambda);
-					
-					double ap = A *(1-(2 * t * epsilon)); // EQ 10
-					
-//					StdOut.printf("ap=%.2f", ap);
-					
-					if(Math.abs(ap) < 0.5){						
-						
-						 int alfa = (int) (250 * ap); // ns es 250?
-						 int beta = (int) (1 * ap); // Es una dimensión di
-						 
-//						 StdOut.printf("alhpa=%d\n", alfa);
-						 
-						 
-						 var aleatorio = StdRandom.uniform(0,250);
-						 
-//						 StdOut.printf("ran=%d\n", aleatorio);
-						 
-//						 var list1 = StdRandom.uniform(250 , alfa); // set de sardinas es igual a ?
-						 
-//						 list1 = StdRandom.uniform(0, 1)*(g.x[1] - list1 + ap);
-						 
-					}else {
-						
-//						 list1 = StdRandom.uniform(0, 1)*(g.x[1] - list1 + ap);
-						
-					}
-					
-					
-					
-					
-				} while (!p.isFeasible());
-				if (p.isBetterThanPBest()) // CUAL SE OCUPA ISBETTERTHAN O isBetterThanPBest 
-					p.updatePBest();
-				swarm.get(i).copy(p);
-			}
-			for (int i = 0; i < ps; i++)
-				if (swarm.get(i).isBetterThan(g))
-					g.copy(swarm.get(i));
 
-			log(t);
-			t++;
-		}
-	
-	}
-	
-//	Mostrar en pantalla 
-	
-	private void log(int t) {
-		StdOut.printf("t=%d,\t%s\n", t, g);
-	}
-	
+    //variables
+    private final int pSF = 100; //20
+    private final int pSD = 400; //30
+    private final int T = 1000;
+    private final int A = 4;
+    private final double epsilon = 0.01;
+    private final double PD = 1 - (pSF / (pSF + pSD));
+    private double lamda;
 
+    //private java.util.List<Pez> swarm = null;
+    private java.util.List<Pez> swarmSF = null;
+    private java.util.List<Pez> swarmSD = null;
+
+    private Pez gSF = null;
+    private Pez gSD = null;
+
+    public void execute() {
+        init();
+        evolve();
+    }
+
+    private void init() {
+        //swarm = new java.util.ArrayList<>();
+        swarmSF = new java.util.ArrayList<>();
+        swarmSD = new java.util.ArrayList<>();
+
+        gSF = new Pez();
+        gSD = new Pez();
+
+        Pez b;
+        for (int i = 1; i <= pSF; i++) {
+            do {
+                b = new Pez();
+            } while (!b.isFeasible()); //si no es factible se mantiene iterando
+            swarmSF.add(b);
+        }
+        for (int i = 1; i <= pSD; i++) {
+            do {
+                b = new Pez();
+            } while (!b.isFeasible()); //si no es factible se mantiene iterando
+            swarmSD.add(b);
+        }
+
+        gSF.copy(swarmSF.get(0));
+        for (int i = 1; i < pSF; i++) {
+            if (swarmSF.get(i).isBetterThan(gSF)) {
+                gSF.copy(swarmSF.get(i));
+            }
+        }
+
+        gSD.copy(swarmSD.get(0));
+        for (int i = 1; i < pSD; i++) {
+            if (swarmSD.get(i).isBetterThan(gSD)) {
+                gSD.copy(swarmSD.get(i));
+            }
+        }
+
+        log(0);
+    }
+
+    private void evolve() {
+
+        int t = 1;
+        while (t <= T) {
+            lamda = 2 * StdRandom.uniform() * PD - PD;
+            Pez p = new Pez();
+            double ataque = A * (1 - (2 * t * epsilon)); // EQ 10 ataque
+
+            if (ataque < 0.5) {
+                
+                double alpha=pSD*ataque;
+                double betha=1*ataque;
+                
+                int i = 0;
+                while (i < pSF) { //puede ser for igual ya tengo la restricción
+                    
+                           //ver donde va esta variable. 
+                    do {
+
+                        //p.SFmove(A, epsilon, t, gSF, gSD, lamda);
+                        p.SFmove(A, epsilon, t, gSF, gSD, lamda, alpha);
+                        p.copy(swarmSF.get(i));
+
+                    } while (!p.isFeasible());
+                    if (swarmSF.get(i).isBetterThan(gSF)) {
+                        gSF.copy(swarmSF.get(i));
+                    }
+                    i++;
+                }
+
+            } else {
+                int j = 0;
+                while (j < pSD) {
+                    
+                    do {
+                        
+                        p.SDmove(A, epsilon, t, gSF, gSD, lamda, ataque);
+                        p.copy(swarmSD.get(j));
+
+                    } while (!p.isFeasible());
+                    if (swarmSD.get(j).isBetterThan(gSD)) {
+                        gSD.copy(swarmSD.get(j));
+                    }
+                    j++;
+                }
+
+                log(t);
+                t++;
+            }
+
+        }
+    }
+    private void log(int t) {
+        StdOut.printf("t=%d,\t%s\n", t, gSF); //aca muestra el objeto g mostrando el metodo toString formateado
+    }
 }
